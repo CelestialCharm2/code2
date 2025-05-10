07431d075f179f382de795cb24f35afc6445b768
CelestialCharm2 <robloxsabersimaccs@gmail.com>
2025-05-10 18:46:19 -0400

    Auto-sync from Roblox Studio

---
+++ Script (5).lua
@@ -0,0 +1,24 @@
+while true do
+wait(1)
+script.Parent.Transparency = (1)
+script.Parent.PointLight.Enabled = false
+wait(3)
+script.Parent.Transparency = (0)
+script.Parent.PointLight.Enabled = true
+wait(9)
+script.Parent.Transparency = (1)
+script.Parent.PointLight.Enabled = false
+wait(4)
+script.Parent.Transparency = (0)
+script.Parent.PointLight.Enabled = true
+script.Parent.Close:Play()
+wait(0.5)
+script.Parent.Transparency = (1)
+script.Parent.PointLight.Enabled = false
+wait(2)
+script.Parent.Transparency = (0)
+script.Parent.PointLight.Enabled = true
+--script.Parent.Strike:Play()
+--script.Parent.StrikeFar:Play()
+wait()
+end
