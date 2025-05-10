5f38a5d8bb514180229cfa1dbe7e7378ff9d622e
CelestialCharm2 <robloxsabersimaccs@gmail.com>
2025-05-10 18:42:33 -0400

    Auto-sync from Roblox Studio

---
+++ Audios.lua
@@ -0,0 +1,200 @@
+ local Audios = {
+	
+	-- Battle's
+	BattleMusic = 18721753476,
+	
+	-- Gyms
+	gym1 = 18721842635,
+	gym2 = 76296596023868,
+	gym3 = 87616652039065,
+	gym4 = 89967315976288,
+
+	-- Routes
+	route1 = 18721717726,
+	route2 = 18721811967,
+	route3 = 18721811967,
+	route4 = 18721811967,
+	route5 = 122483922396114,
+	route6 = 17475168089,
+	route7 = 116180532768892,
+	route8 = 17475172128,
+	route9 = 17475171297,
+	route10 = 17475171297,
+	route11 = 17475167398,
+	route12 = 17475167398,
+	route13 = 17475165693,
+	route14 = 17475165004,
+	route15 = 17475170652,
+	route16 = 17475169890,
+	route17 = 17475173758,
+	route18 = 17475173040,
+
+	-- Chunks
+	chunk28 = 17475269563,
+	chunk29 = 17475269563,
+	chunk30 = 17475269563,
+	chunk31 = 17475269563,
+	chunk32 = 17475269563,
+	chunk33 = 17475269563,
+	chunk34 = 17475269563,
+
+	-- Elite 4
+	eliteIce = 0,
+	eliteFighting = 0,
+	eliteDragon = 0,
+	elitechampionhall = 0,
+	eliteFairy = 0,
+
+	-- Roria League
+	RLsafariZone = 0,
+	RLEncounterPlace = 0,
+	RLentrance = 0,
+
+
+	-- Decca
+	PortDecca = 17475334852,
+	DeccaBeach = 17475334008,
+
+
+	-- Anthian city
+	AnthianSewer = 17475224444,
+	AnthianCityPD = 17475227745,
+	AnthianCityBD = 17475226300,
+	AnthianCitySD2 = 17475227105,
+	AnthianCitySD1 = 17475225187,
+	AnthianCityHD = 17475228518,
+
+
+	-- Gale
+	GaleForest1 = 18721808473,
+	GaleForest2 = 18721808473,
+
+
+	-- Lagoona
+	LagoonaLake1 = 17475213328,
+	LagoonaLake2 = 17475212670,
+	LagoonaTrenches = 17475212075,
+
+	-- Aredia
+	ArediaCity1 = 17475270163,
+	ArediaCity2 = 17475268330,
+	OldAredia = 17475743087,
+
+	-- Voltridia
+	VoltridiaCavern = 0,
+	VoltridiaIsland = 0,
+
+	-- Palace
+	Palace2 = 17475274591,
+	Palace1 = 17475273739,
+
+	-- Silvent
+	SilventCity1 = 18721838057,
+	SilventCity2 = 18721838057,
+
+
+	-- Rosecove
+	RosecoveCity = 17475203282,
+	RosecoveBeach = 17475206691,
+
+	-- Cragonos
+	CragonosSpring = 0,
+	CragonosSanctuary = 0,
+	CragonosMines = 17475218308,
+	CragonosCliffs = 0,
+	CragonosPeak = 17475217668,
+
+	-- Fluoruma
+	FluorumaCity = 17475278970,
+	FluorumaCityGym = 17475286139,
+
+
+	-- Secret
+	SecretLab = 14501607371,
+	SecretGrove = 14501607371,	
+
+
+	-- Frigidia
+	FrigidiaCavern = 0,
+	FrigidiaIsland = 0,
+
+
+	-- Frostveil
+	FrostveilCityCatacombs = 17475788244,
+	FrostveilCity = 17475286139,
+	FrostveilCityGym = 17475286139,
+
+
+	-- Fortulos
+	FortuloseManor1 = 17475419335, --Inside , I guessed 1 is Inside & 2 is Outside - Noob
+	FortuloseManor2 = 17475418677, --Outside
+
+	-- Mitis
+	mitis1 = 18721712920,
+	mitis2 = 18721712920,
+
+
+	-- Eclipse Base
+	EclipseBaseEntranceHall = 17475352516,
+	EclipseBaseCafeteria = 17475352516,
+	EclipseBasePowerStation = 17475352516,
+	EclipseBaseLivingQuarters = 17475352516,
+	EclipseBaseSurveillanceRoom = 17475352516,
+	EclipseBaseCypressOffice = 17475352516,
+	EclipseBaseCypressOfficePrisonCells = 17475352516,
+	EclipseBaseAircraftHangar = 17475352516,
+
+
+	-- Battle Colosseum
+	BattleColosseum = 18319891265,
+
+	-- Trade resort
+	TradeResort1 = 18319892962,
+	TradeResort2 = 18319892962,
+
+	-- Obsidia
+	ObsidiaCavern = 0,
+	ObsidiaIsland = 0,
+
+
+	-- Other Locations
+	CrescentTownGym = 14501397059,
+	OceanSOrigin = 17475335330,
+	AborilleOutpost = 17475344000,
+	DemonsTomb = 0,
+	GeneLab = 0,
+	CrescentTown = 17475343169,
+	ShadowVoid = 14501623719,
+	SilverCove = 17475205115,
+	LostIslands = 17475335914,
+	LostIslandsDJ = 14501208572,
+	PathofTruth = 17475205974,
+	SteamChamber = 14501840921,
+	CosmeosValley = 17475219207,
+	magikpond = 0,
+	TinbellConstructionSite = 0,
+	TinbellTower = 0,
+	FreezingFissure = 17475284064,
+	DendriteChamber = 14501840921,
+	CosmeosObservatory = 14501853832,
+	TitansThrong = 0,
+	MartensiteChamber = 14501840921,
+	CalciteChamber = 14501840921,
+	ChamberoftheJewel = 14501840921,
+	IgneusDepths = 17475189991,
+	NaturesDen = 17475280382,
+	CheshmaTown = 18721805244,
+	OldGraveyard = 17475186181,
+	BrimberCity = 17475188680,
+	GroveofDreams = 17475202404,
+	GlisteningGrotto = 17475279729,
+	DesertCatacombs = 17475269563,
+	GoldenPokeballArcade = 14501725248,
+	LudiLoco = 17475232137,
+	MtIgneus = 17475189991,
+	HMFoyer = 0,
+	HMAttic = 0,
+	Gate11 = 17475439649,
+}
+
+return Audios
