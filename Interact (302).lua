29fa7e781306eacfa66e738b154fb7f9b0b89fe2
CelestialCharm2 <robloxsabersimaccs@gmail.com>
2025-05-10 18:45:58 -0400

    Auto-sync from Roblox Studio

---
+++ Interact (302).lua
@@ -0,0 +1,9 @@
+return {
+	"The creation of DIALGA,",
+	"The giver of time...",
+	"In laughter, there is tears...",
+	"And, likewise it is with time.",
+	"The same time flows on.",
+	"For it is the blessing of DIALGA.",
+	"- A statue replica of DIALGA, designed after the statues in Eterna City, Sinnoh.",
+}
