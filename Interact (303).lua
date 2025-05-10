67925ca6572953ea638720dc703c766d82957c67
CelestialCharm2 <robloxsabersimaccs@gmail.com>
2025-05-10 18:45:59 -0400

    Auto-sync from Roblox Studio

---
+++ Interact (303).lua
@@ -0,0 +1,9 @@
+return {
+	"The birth of PALKIA.",
+	"The creator of parallel dimensions...",
+	"Alive, yet not alive...",
+	"Everything drifts in space...",
+	"To arrive in the same universe.",
+	"It is the blessing of PALKIA.",
+	"- A statue replica of PALKIA, designed after the statues in Eterna City, Sinnoh.",
+}
