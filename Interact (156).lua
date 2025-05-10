d7b0f0600466c1d986a5cea1e939a97fa4fb13c9
CelestialCharm2 <robloxsabersimaccs@gmail.com>
2025-05-10 18:44:35 -0400

    Auto-sync from Roblox Studio

---
+++ Interact (156).lua
@@ -0,0 +1,16 @@
+return {'This large tree actually has quite a lot of history.',
+	'My father would tell me that this tree was a symbol of life.',
+	'Long ago a terribly strong pokemon wreaked havoc over this part of the land.',
+	'Then a pokemon from the woods appeared and battled the wicked pokemon.',
+	'It defeated the evil Pokemon of destruction and brought balance to this land.',
+	'Before it left, it planted this tree to stand over the whole forest and protect it.',
+	'That\'s quite a tale, isn\'t it?'}
+
+--return {'Ancient stories tell of life, death and the order that balances them.',
+--	'This tree is known as "Yggdrasil", the life in the world stems from it.',
+--	'Long ago a mysterious Pokemon awoke, to counter a foul evil summoned by the "Ultimate Weapon" that rests at the bottom of Route 16.',
+--	'Thus awoke order, it constructed and put down the foul evil, in this very forest the life took shape into this very tree, legend tells it lays dormant inside.',
+--	'I heard whispers of its study in the Anthian Sewers, apparently its due to awake again.',
+--	--NPC WILL THEN GIVE YOU A KEYCARD TO YVELTALS ROOM
+--	'Take this, go see for yourself.'
+--}
