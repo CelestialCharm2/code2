27d744c4455e47a2dc8eea57f72ec95682ee0540
CelestialCharm2 <robloxsabersimaccs@gmail.com>
2025-05-10 18:42:32 -0400

    Auto-sync from Roblox Studio

---
+++ Settings (1).lua
@@ -0,0 +1,73 @@
+return {
+	GroupId = 35034961,
+	GroupTags = {
+		["255"] = {
+			Text = "🛠️| Holder",
+			Color = Color3.fromRGB(0, 0, 0),
+		},
+		["254"] = {
+			Text = "🤴 | Founder",
+			Color = Color3.fromRGB(0, 0, 128),
+		},
+		["253"] = {
+			Text = "👑 | Owner",
+			Color = Color3.fromRGB(224, 184, 234),
+		},
+		["252"] = {
+			Text = "👮 | Staff Manager",
+			Color = Color3.fromRGB(224, 184, 234),
+		},
+		["251"] = {
+			Text = "👮 | Community Manager",
+			Color = Color3.fromRGB(255, 140, 0),
+		},
+		["250"] = {
+			Text = "👮 | Head of Administration",
+			Color = Color3.fromRGB(0, 255, 0),
+		},
+		["249"] = {
+			Text = "👮 | Head of Moderation",
+			Color = Color3.fromRGB(0, 191, 255),
+		},
+		["248"] = {
+			Text = "👮 | Senior Developer",
+			Color = Color3.fromRGB(0, 191, 255),
+		},
+		["247"] = {
+			Text = "🛠️ | Developer",
+			Color = Color3.fromRGB(0, 191, 255),
+		},
+		["246"] = {
+			Text = "👮 | Senior Administrator",
+			Color = Color3.fromRGB(0, 191, 255),
+		},
+		["245"] = {
+			Text = " 📋 | Administrator",
+			Color =  Color3.fromRGB(0, 255, 0),
+		},
+		["244"] = {
+			Text = " 🎮 | Senior Moderator",
+			Color =  Color3.fromRGB(255, 140, 0),
+		},
+		["243"] = {
+			Text = "🛡️ | Moderator",
+			Color = Color3.fromRGB(0, 191, 255),
+		},
+		["242"] = {
+			Text = "🛡️ | Trial Moderator",
+			Color = Color3.fromRGB(148, 0, 211),
+		},
+		["3"] = {
+			Text = "📷 | Content Creator",
+			Color = Color3.fromRGB(28, 221, 18),
+		},
+		["2"] = {
+			Text = "💎 | Server Booster",
+			Color = Color3.fromRGB(28, 221, 18),
+		},
+		["1"] = {
+			Text = "🌎 | Trainer",
+			Color = Color3.fromRGB(47, 255, 44),
+		},
+	}
+}
